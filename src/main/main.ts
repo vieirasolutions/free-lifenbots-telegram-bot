05d95ec1258eb50a2fbf93cddabfd63b7c09b7d0
Breno Vieira Soares <breno@lifenpag.com.br>
2021-09-19 21:28:11 -0300

    feat: add new command

--- src/main/main.ts
+++ src/main/main.ts
@@ -4,13 +4,20 @@
 'use strict'
 
 import { config } from 'dotenv'
-import { Telegraf } from 'telegraf'
+import { Telegraf, Markup } from 'telegraf'
 
 // Init .env file data
 config()
 
 // Create instance of a bot with telegraf lib
 const bot = new Telegraf(process.env.BOT_KEY)
+
+const keyboard = Markup.inlineKeyboard([
+  Markup.button.url('Acessar Robôs', 'https://app.lifenbot.com'),
+  Markup.button.url('Acessar Aulas', 'https://lifenbot.com/cadastro-portal')
+])
+
+bot.command('acessos', async (ctx) => await ctx.reply('*Para acessar os robôs:* \nClique abaixo no botão "Acessar Robôs"\nEmail: *Seu email*\nSenha: *senhaprovisoria*', { parse_mode: 'MarkdownV2', ...keyboard }))
 
 bot.on('new_chat_members', async (ctx) => {
   ctx.update.message.new_chat_members.map(async (member) => {
